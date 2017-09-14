eef0bfacbd94d97f1fd9d3930601d77d40279cf8
Mateus Rodrigues <devrdgs@gmail.com>
2017-09-14 19:47:52 -0300

    Fixed sidebar after content init

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,34 +1,51 @@
+import { AfterContentInit, ChangeDetectorRef, Component, OnInit, OnChanges } from '@angular/core';
+
+import { Subscription } from 'rxjs/Subscription';
+
 import { AuthService } from './../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { ResidenceEmitter } from './../../emitters/residence.emitter';
 
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.styl']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent implements OnInit, AfterContentInit {
 
+  private _enteredResidenceSubscription: Subscription;
   _id: string;
   idResidence: string;
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _cdr: ChangeDetectorRef,
+    private _residenceEmitter: ResidenceEmitter
   ) { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterContentInit() {
     this.getIdAccount();
-    this.getIdResidence();
+    this.idResidence = this._authService.getToken('lastEnteredResidence');
+    if (this.idResidence === '') {
+      this.startResidenceSubscription();
+    }
   }
 
   getIdAccount() {
     this._id = this._authService.getDataFromToken('_id');
   }
 
-  getIdResidence() {
-    const token = this._authService.getToken('lastEnteredResidence');
-    if (token) {
-      this.idResidence = token;
-    }
+  startResidenceSubscription() {
+    this._enteredResidenceSubscription =
+      this._residenceEmitter
+          .enteredResidence
+          .subscribe(idResidence => {
+            this.idResidence = idResidence;
+            this._cdr.detectChanges();
+          });
   }
 
 }
